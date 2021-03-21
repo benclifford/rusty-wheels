ea94f9c1c6e0b512a6aff9bfbc513292a2734080
Ben Clifford <benc@hawaga.org.uk>
2021-03-21 18:12:40 +0000

    Make magnet poll messages more specific to magnet

--- src/magnet.rs
+++ src/magnet.rs
@@ -13,7 +13,7 @@ impl Magnet {
     pub fn pulsed(&mut self) -> bool {
         match self.poller.poll(0) {
             Ok(Some(value)) => {
-                println!("Poll got a value {}", value);
+                println!("Magnet poll got a pulse (value {})", value);
                 true
             }
             _ => false,
@@ -30,7 +30,7 @@ fn setup_magnet() -> std::result::Result<sysfs_gpio::PinPoller, sysfs_gpio::Erro
     let mut poller: sysfs_gpio::PinPoller = pin.get_poller()?;
     println!("Making first pin poll");
     match poller.poll(0)? {
-        Some(value) => println!("Poll got first value {} - ignoring", value),
+        Some(value) => println!("Magnet poll got first value {} - ignoring", value),
         None => (),
     }
     println!("Done configuring magnet");
