c54d30403f527e94e12e3be32b7990297bbaa8af
Ben Clifford <benc@hawaga.org.uk>
2021-03-27 11:31:43 +0000

    Make Jumbler generic over Copyable types, rather than over modes

--- src/jumble.rs
+++ src/jumble.rs
@@ -1,32 +1,21 @@
 use rand::seq::SliceRandom;
 use rand::Rng;
 
-use crate::structs::Mode;
-
-/// I feel like this should be generic over the content type
-/// but I couldn't get the generics working right with requiring
-/// the content be Copy (so I can both return it and keep it in
-/// the list.
-///
-/// TODO: this jumble method doesn't give the second half of the
-/// initial list a fair chance to be picked. Maybe the content
-/// list should be shuffled at creation time?
-
-pub struct Jumbler<const LEDS: usize> {
-    content: Vec<fn() -> Box<dyn Mode<LEDS>>>,
+pub struct Jumbler<T> {
+    content: Vec<T>
 }
 
-impl<const LEDS: usize> Jumbler<LEDS> {
-    pub fn new(mut content: Vec<fn() -> Box<dyn Mode<LEDS>>>) -> Jumbler<LEDS> {
+impl<T> Jumbler<T> {
+    pub fn new(mut content: Vec<T>) -> Jumbler<T> {
         content.shuffle(&mut rand::thread_rng());
         Jumbler { content: content }
     }
 }
 
-impl<const LEDS: usize> Iterator for Jumbler<LEDS> {
-    type Item = fn() -> Box<dyn Mode<LEDS>>;
+impl<T: Copy> Iterator for Jumbler<T> {
+    type Item = T;
 
-    fn next(&mut self) -> Option<fn() -> Box<dyn Mode<LEDS>>> {
+    fn next(&mut self) -> Option<T> {
         let next_index = rand::thread_rng().gen_range(0, self.content.len() / 2);
 
         let entry = self.content.remove(next_index);
