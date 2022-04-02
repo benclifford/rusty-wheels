af0828b3aa98e2240ca4eaa9027f24fa02d77df2
Ben Clifford <benc@hawaga.org.uk>
2022-04-02 17:52:59 +0100

    Add complement_sides chill mode

--- src/chill_modes.rs
+++ src/chill_modes.rs
@@ -4,14 +4,15 @@ use crate::helpers::fraction_to_rgb;
 use crate::leds::{Side, WheelLEDs};
 use crate::structs::FrameState;
 
-const MODE_CHANGE_SEC: u64 = 10;
+const MODE_CHANGE_SEC: u64 = 60;
 
 fn chill_modes<const LEDS: usize>() -> &'static [for<'r, 's> fn(
     side: Side,
     &'r mut WheelLEDs<LEDS>,
     &'s FrameState,
 ) -> Result<(), std::io::Error>] {
-    &[rainbow]
+    &[rainbow,
+      complement_sides]
 }
 
 pub fn render_chill_mode<const LEDS: usize>(
@@ -36,6 +37,29 @@ fn rainbow<const LEDS: usize>(
     for led in 0..LEDS {
         let phase = (led as f32) / (LEDS as f32);
         let rgb = fraction_to_rgb(phase, Some(0.5));
+        wheel_leds.set(side, led, rgb);
+    }
+
+    Ok(())
+}
+
+fn complement_sides<const LEDS: usize>(
+    side: Side,
+    wheel_leds: &mut WheelLEDs<LEDS>,
+    framestate: &FrameState,
+) -> io::Result<()> {
+    let side_phase = if side == Side::Left { 0.0 } else { 0.5 };
+
+    let now_ms = framestate.now.as_millis();
+    let now_steps = (now_ms as f32) / 30000.0;
+
+    let time_phase = now_steps % 1.0;
+
+    let phase = (time_phase + side_phase) % 1.0;
+
+    let rgb = fraction_to_rgb(phase, Some(0.5));
+
+    for led in 0..LEDS {
         wheel_leds.set(side, led, rgb);
     }
 
